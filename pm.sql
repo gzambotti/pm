4d1af9c9af2f7b7023d9f5713fa58e1970f30a21
Giovanni Zambotti <g.zambotti@gmail.com>
2017-02-22 14:54:53 -0500

    removed constraint

--- pm.sql
+++ pm.sql
@@ -20,41 +20,41 @@ ALTER TABLE blockgrp ALTER COLUMN geom TYPE geometry(MultiPolygon,2163) USING ST
 -- QD' PM2.5 predictions (make sure that in the csv the lng column is before the lat column
 -- if this is not true, you need to change the create table statement)
 
-create table pm25 (gid serial NOT NULL, lat double precision, lng double precision, pm double precision);
+CREATE TABLE pm25 (gid serial NOT NULL, lat double precision, lng double precision, pm double precision);
 
 COPY pm25 from '/path/PM25_Predictions_New_England/pm25.csv' DELIMITERS ',' CSV header;
 
-alter table pm25 add column geom geometry (Point, 4236);
+ALTER TABLE pm25 add column geom geometry (Point, 4236);
 
-update pm25 set geom = ST_SetSRID(ST_MakePoint(lng,lat), 4236);
+UPDATE pm25 SET geom = ST_SetSRID(ST_MakePoint(lng,lat), 4236);
 
 ALTER TABLE pm25 ALTER COLUMN geom TYPE geometry(Point, 2163) USING ST_Transform(geom, 2163);
 
-ALTER TABLE pm25 add CONSTRAINT pm25_pkey PRIMARY KEY (gid)
+--ALTER TABLE pm25 add CONSTRAINT pm25_pkey PRIMARY KEY (gid)
 
 CREATE INDEX pm25_gix ON pm25 USING GIST (geom);
 
 -- Import PO boxes (make sure that in the csv the lng column is before the lat column
 -- if this is not true, you need to change the create table statement)
 
-create table pobox (gid serial NOT NULL, zip character varying, enc_zip character varying, state character varying, area character varying, po_name character varying, nametype character varying, cty1fips character varying, cty2fips character varying, cty3fips character varying, ropo_flag character varying, zip_type character varying, lng double precision, lat double precision);
+CREATE TABLE pobox (gid serial NOT NULL, zip character varying, enc_zip character varying, state character varying, area character varying, po_name character varying, nametype character varying, cty1fips character varying, cty2fips character varying, cty3fips character varying, ropo_flag character varying, zip_type character varying, lng double precision, lat double precision);
 
 COPY pobox from '/path/Predicted_PM25/PO_boxes.csv' DELIMITERS ',' CSV header;
 
-alter table pobox add column geom geometry (Point, 4236);
+ALTER TABLE pobox add column geom geometry (Point, 4236);
 
-update pobox set geom = ST_SetSRID(ST_MakePoint(lng,lat), 4236);
+UPDATE pobox SET geom = ST_SetSRID(ST_MakePoint(lng,lat), 4236);
 
 ALTER TABLE pobox ALTER COLUMN geom TYPE geometry(Point, 2163) USING ST_Transform(geom, 2163);
 
-ALTER TABLE pobox add CONSTRAINT pobox_pkey PRIMARY KEY (gid)
+--ALTER TABLE pobox add CONSTRAINT pobox_pkey PRIMARY KEY (gid)
 
 CREATE INDEX pobox_gix ON pobox USING GIST (geom);
 
 -- Task 1: Calculate the PM average by zipcode with a buffer of 1000 (unit meters):
 
-select zipcode.gid, sum(pm)/count(*) as averagepm, zip FROM pm25, zipcode
-where ST_DWithin(zipcode.geom, pm25.geom, 1000) GROUP BY zipcode.gid;
+SELECT zipcode.gid, sum(pm)/count(*) AS averagepm, zip FROM pm25, zipcode
+WHERE ST_DWithin(zipcode.geom, pm25.geom, 1000) GROUP BY zipcode.gid;
 
 -- Task 2: Calculate the PM average by block group, include censusid, zipcode, population, latlng fields to the table.
 
