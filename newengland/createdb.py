f1f2be88385186d5cc27119170846fd1488fc999
Giovanni Zambotti <g.zambotti@gmail.com>
2018-01-24 18:02:27 -0500

    create a db and postgis extension

---
+++ newengland/createdb.py
@@ -0,0 +1,32 @@
+import psycopg2
+from psycopg2 import connect
+import sys
+from psycopg2.extensions import ISOLATION_LEVEL_AUTOCOMMIT
+
+# choose a database name
+dbname = "test"
+
+def createdb():
+        conn_string = "host='localhost' user='postgres' password='postgres'"
+        conn = psycopg2.connect(conn_string)
+        conn.set_isolation_level(ISOLATION_LEVEL_AUTOCOMMIT)
+        cursor = conn.cursor()        
+        cursor.execute('CREATE DATABASE ' + dbname)
+        conn.commit()
+        cursor.close()
+        conn.close()
+
+
+def createpostgisext():
+        conn_string = "host='localhost' dbname='" + dbname + "' user='postgres' password='postgres'"
+        conn = psycopg2.connect(conn_string)
+        conn.set_isolation_level(ISOLATION_LEVEL_AUTOCOMMIT)
+        cursor = conn.cursor()
+        cursor.execute('CREATE EXTENSION postgis;')
+        conn.commit()
+        cursor.close()
+        conn.close()
+
+if __name__ == "__main__":
+    createdb()
+    createpostgisext()
